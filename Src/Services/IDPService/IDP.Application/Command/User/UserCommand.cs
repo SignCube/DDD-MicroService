e8505952f26c55af2a96a9e145756edf8edda802
Ali Bairampour <bairampour@gmail.com>
2024-06-14 02:37:20 +0330

    user command

---
+++ Src/Services/IDPService/IDP.Application/Command/User/UserCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.Application.Command.User
+{
+    public record UserCommand:IRequest<bool>
+    {
+        public required string? Name { get; set; }
+
+    }
+}
